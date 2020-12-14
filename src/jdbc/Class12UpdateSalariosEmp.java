f9d08a8061d2292618e00ffb6923be5b96f62c1c
Serra <pacoserranox@gmail.com>
2020-12-14 20:50:57 +0100

    Merge origin/master

---
+++ src/jdbc/Class12UpdateSalariosEmp.java
@@ -0,0 +1,15 @@
+package jdbc;
+
+/*
+	Vamos a incrementar el salario de los empleados
+por su departamento.
+	Pediremos el NOMBRE del departamento
+	Actualizaremos los datos de los empleados del departamento.
+Empleados modificados: 6
+	Preguntaremos de nuevo si queremos modificar.
+NO ES NECESARIO MOSTRAR LOS REGISTROS (opcional)
+Versión 2: Mostramos los empleados modificados
+ */
+public class Class12UpdateSalariosEmp {
+
+}
