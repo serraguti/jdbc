8e444035cb5d52d1544a4ff57b0730e7b21c36ab
Serra <pacoserranox@gmail.com>
2020-12-16 20:04:23 +0100

    Merge origin/master

--- src/jdbc/Class18EliminarPlantilla.java
+++ src/jdbc/Class18EliminarPlantilla.java
@@ -1,6 +1,7 @@
 package jdbc;
 
 //<editor-fold defaultstate="collapsed" desc="PROCEDIMIENTO OUT">
+import com.mysql.cj.jdbc.Driver;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -25,17 +26,35 @@ end;
  */
 //</editor-fold>
 public class Class18EliminarPlantilla {
+//'amigo r.'
 
+//DELIMITER $$
+//CREATE PROCEDURE eliminarplantilla (IN p_apellido varchar(30)
+//, OUT personas int, OUT suma int)
+//BEGIN
+//	delete from plantilla
+//	where lower(apellido)=lower(p_apellido);
+//	select count(empleado_no), sum(salario)
+//	into personas, suma
+//	from plantilla;
+//END$$
     public static void main(String[] args) throws SQLException {
         Scanner teclado = new Scanner(System.in);
         System.out.println("APP PROCEDURE OUT PLANTILLA");
         System.out.println("---------------------------");
         System.out.println("Introduzca apellido");
         String apellido = teclado.nextLine();
+        /*
         DriverManager.registerDriver(new OracleDriver());
         String cadena = "jdbc:oracle:thin:@LOCALHOST:1521:XE";
         Connection cn
                 = DriverManager.getConnection(cadena, "system", "oracle");
+         */
+        DriverManager.registerDriver(new Driver());
+        String cadenaconexion = "jdbc:mysql://localhost:3306/hospital";
+        Connection cn = DriverManager.getConnection(
+                cadenaconexion,
+                "root", "mysql");
         String consultaproc = "{ call eliminarplantilla (?,?,?) }";
         CallableStatement cst = cn.prepareCall(consultaproc);
         cst.setString(1, apellido);
---
+++ src/jdbc/Class19UpdateDoctores.java
@@ -0,0 +1,60 @@
+package jdbc;
+
+//<editor-fold defaultstate="collapsed" desc="PROCEDURE OUT">
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+import oracle.jdbc.OracleDriver;
+
+/*
+create or replace procedure updatesalariodoctores
+(p_nombre hospital.nombre%type
+, p_incremento int
+, p_suma out int
+, p_media out int)
+as
+  v_codigo int;
+begin
+  --debemos guardar el codigo del hospital
+  --para el update de los doctores
+  select hospital_cod into v_codigo
+  from hospital
+  where lower(nombre)=lower(p_nombre);
+  update doctor set salario = salario + p_incremento
+  where hospital_cod=v_codigo;
+  commit;
+  select sum(salario), avg(salario) into p_suma, p_media
+  from doctor
+  where hospital_cod=v_codigo;
+end;
+ */
+//</editor-fold>
+public class Class19UpdateDoctores {
+
+    public static void main(String[] args) throws SQLException {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca nombre de hospital");
+        String nombre = teclado.nextLine();
+        System.out.println("Incremento salarial");
+        String datoincremento = teclado.nextLine();
+        int incremento = Integer.parseInt(datoincremento);
+        DriverManager.registerDriver(new OracleDriver());
+        String cadena = "jdbc:oracle:thin:@LOCALHOST:1521:XE";
+        Connection cn
+                = DriverManager.getConnection(cadena, "system", "oracle");
+        String consultaproc = "{ call updatesalariodoctores (?,?,?,?) }";
+        CallableStatement cst = cn.prepareCall(consultaproc);
+        cst.setString(1, nombre);
+        cst.setInt(2, incremento);
+        cst.registerOutParameter(3, java.sql.Types.INTEGER);
+        cst.registerOutParameter(4, java.sql.Types.INTEGER);
+        cst.executeUpdate();
+        int suma = cst.getInt(3);
+        int media = cst.getInt(4);
+        System.out.println("La media del hospital " + nombre + " es "
+                + media + " y su suma salarial es " + suma);
+        cn.close();
+    }
+}
